a7db4f87ea718ad260ffa52cd8ce2dc2fd42a98f
Fardosa <muniiramohamed096@gmail.com>
2024-01-02 17:46:46 +0300

    added a sidebar

---
+++ script.js
@@ -0,0 +1,6 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+// var menuIcon = document.querySelector(".menu-icon");
+menuIcon.onclick =function(){
+    sidebar.classList.toggle("small-sidebar");
+}
